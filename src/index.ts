3fddc8dc83e4e2dcaae7a232c110bfa00f06d578
Michael Hayes <michael@hayes.io>
2023-04-10 10:55:31 -0700

    init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { createYoga } from 'graphql-yoga';
+import { createServer } from 'node:http';
+import SchemaBuilder from '@pothos/core';
+
+const builder = new SchemaBuilder({});
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string(),
+      },
+      resolve: (parent, { name }) => `hello, ${name || 'World'}`,
+    }),
+  }),
+});
+
+const yoga = createYoga({
+  schema: builder.toSchema(),
+});
+
+const server = createServer(yoga);
+
+server.listen(8000, () => {
+  console.log('Visit http://localhost:8000/graphql');
+});
